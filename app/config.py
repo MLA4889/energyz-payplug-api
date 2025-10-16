c5a0613ee89e58909fd71e03895d000b99743eee
MLA4889 <mohammed.lamachi@energyz.fr>
2025-10-16 18:03:28 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,7 @@
-import os
-import json
+import os, json
 
 class Settings:
-    BRAND_NAME = "ENERGYZ"
+    BRAND_NAME = os.getenv("BRAND_NAME", "ENERGYZ")
     MONDAY_API_KEY = os.getenv("MONDAY_API_KEY")
     MONDAY_BOARD_ID = int(os.getenv("MONDAY_BOARD_ID", "0"))
 
@@ -10,23 +9,16 @@ class Settings:
     ADDRESS_COLUMN_ID = os.getenv("ADDRESS_COLUMN_ID")
     IBAN_FORMULA_COLUMN_ID = os.getenv("IBAN_FORMULA_COLUMN_ID")
 
-    FORMULA_COLUMN_IDS = json.loads(os.getenv("FORMULA_COLUMN_IDS", "{}"))
-    LINK_COLUMN_IDS = json.loads(os.getenv("LINK_COLUMN_IDS", "{}"))
-    STATUS_COLUMN_ID = os.getenv("STATUS_COLUMN_ID")
+    FORMULA_COLUMN_IDS = json.loads(os.getenv("FORMULA_COLUMN_JSON", "{}"))
+    LINK_COLUMN_IDS = json.loads(os.getenv("LINK_COLUMN_JSON", "{}"))
+    STATUS_AFTER_PAY = json.loads(os.getenv("STATUS_AFTER_PAY_JSON", "{}"))
 
-    PAYPLUG_TEST_KEY = os.getenv("PAYPLUG_TEST_KEY")
-    PAYPLUG_LIVE_KEY = os.getenv("PAYPLUG_LIVE_KEY")
+    PAYPLUG_KEYS = json.loads(os.getenv("PAYPLUG_KEYS_JSON", "{}"))
+    PAYPLUG_MODE = os.getenv("PAYPLUG_MODE", "test")
 
     EVOLIZ_BASE_URL = os.getenv("EVOLIZ_BASE_URL", "https://api.evoliz.com")
     EVOLIZ_COMPANY_ID = os.getenv("EVOLIZ_COMPANY_ID")
     EVOLIZ_PUBLIC_KEY = os.getenv("EVOLIZ_PUBLIC_KEY")
     EVOLIZ_SECRET_KEY = os.getenv("EVOLIZ_SECRET_KEY")
-
-    STATUS_AFTER_PAY = {
-        "1": "Acompte 1 payé",
-        "2": "Acompte 2 payé",
-        "3": "Acompte 3 payé",
-        "4": "Acompte 4 payé",
-    }
 
 settings = Settings()
