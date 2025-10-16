d87db93392fd393cafe168bdb14d11255e013175
MLA4889 <mohammed.lamachi@energyz.fr>
2025-10-16 22:08:01 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,24 +1,22 @@
-import os, json
+from pydantic import BaseSettings
 
-class Settings:
-    BRAND_NAME = os.getenv("BRAND_NAME", "ENERGYZ")
-    MONDAY_API_KEY = os.getenv("MONDAY_API_KEY")
-    MONDAY_BOARD_ID = int(os.getenv("MONDAY_BOARD_ID", "0"))
 
-    EMAIL_COLUMN_ID = os.getenv("EMAIL_COLUMN_ID")
-    ADDRESS_COLUMN_ID = os.getenv("ADDRESS_COLUMN_ID")
-    IBAN_FORMULA_COLUMN_ID = os.getenv("IBAN_FORMULA_COLUMN_ID")
+class Settings(BaseSettings):
+    MONDAY_API_KEY: str
+    MONDAY_BOARD_ID: str
+    PUBLIC_BASE_URL: str
 
-    FORMULA_COLUMN_IDS = json.loads(os.getenv("FORMULA_COLUMN_JSON", "{}"))
-    LINK_COLUMN_IDS = json.loads(os.getenv("LINK_COLUMN_JSON", "{}"))
-    STATUS_AFTER_PAY = json.loads(os.getenv("STATUS_AFTER_PAY_JSON", "{}"))
+    EVOLIZ_PUBLIC_KEY: str
+    EVOLIZ_SECRET_KEY: str
+    EVOLIZ_COMPANY_ID: str
+    EVOLIZ_BASE_URL: str = "https://www.evoliz.io"
 
-    PAYPLUG_KEYS = json.loads(os.getenv("PAYPLUG_KEYS_JSON", "{}"))
-    PAYPLUG_MODE = os.getenv("PAYPLUG_MODE", "test")
+    PAYPLUG_MODE: str = "test"
+    PAYPLUG_KEYS_JSON: str = "{}"
+    PAYPLUG_KEYS_TEST_JSON: str = "{}"
 
-    EVOLIZ_BASE_URL = os.getenv("EVOLIZ_BASE_URL", "https://api.evoliz.com")
-    EVOLIZ_COMPANY_ID = os.getenv("EVOLIZ_COMPANY_ID")
-    EVOLIZ_PUBLIC_KEY = os.getenv("EVOLIZ_PUBLIC_KEY")
-    EVOLIZ_SECRET_KEY = os.getenv("EVOLIZ_SECRET_KEY")
+    class Config:
+        env_file = ".env"
+
 
 settings = Settings()
